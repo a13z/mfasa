2f0f17ef2ea4302e7852868be16f48a7ffc532ca
Alfonso Gonzalez <alfonsog@gmail.com>
2021-01-24 18:23:03 +0100

    Skeleton pages

---
+++ src/pages/ASAConfig.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ASAConfig = () => (   
+    <h1>ASA Config</h1>
+)
+
+export default ASAConfig;
---
+++ src/pages/ASAManager.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ASAManager = () => (   
+    <h1>ASA Manager</h1>
+)
+
+export default ASAManager;
