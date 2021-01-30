3029d5e72d3bd47df0f2d3c9e55cf778b43da76e
Alfonso Gonzalez <alfonsog@gmail.com>
2021-01-31 00:55:21 +0100

    Increase layout margin

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ const ListLink = props => (
 
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <div style={{ margin: `auto`, maxWidth: 1200, padding: `0 1rem` }}>
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
           <h3 style={{ display: `inline` }}>MFASA</h3>
