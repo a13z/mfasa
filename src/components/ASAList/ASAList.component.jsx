8cdc9036d4f56f99c84737eb16f601786749fce1
Alfonso Gonzalez <alfonsog@gmail.com>
2021-01-31 00:53:09 +0100

    Add table data from api

--- src/components/ASAList/ASAList.component.jsx
+++ src/components/ASAList/ASAList.component.jsx
@@ -2,40 +2,40 @@ import React, {useState, useContext, useEffect} from 'react';
 import { useTable } from 'react-table'
 import { AlgoSignerContext } from "../../contexts/algosigner.context";
 
+import ASATransactions from "../ASATransactions/ASATransactions.component";
+
 import AlgoSdk from '../../services/AlgoSdk'
 import axios from 'axios';
 
+
 const ASAList = (address) => {
-    const [accountDetails, setAccountDetails] = useState({})
+    const [accountDetails, setAccountDetails] = useState({});
+    const [createdAssets, setCreatedAssets] = useState([]);
     const algoSignerContext = useContext(AlgoSignerContext);
+    const [loading, setLoading] = useState(false);
 
     const data = React.useMemo(
-    () => [
-        {
-        col1: 'Hello',
-        col2: 'World',
-        },
-        {
-        col1: 'react-table',
-        col2: 'rocks',
-        },
-        {
-        col1: 'whatever',
-        col2: 'you want',
-        },
-    ],
-    []
+    () => createdAssets,
+    [createdAssets]
     )
 
     const columns = React.useMemo(
     () => [
         {
+        Header: 'Index',
+        accessor: 'index', // accessor is the "key" in the data
+        },
+        {
         Header: 'Name',
-        accessor: 'name', // accessor is the "key" in the data
+        accessor: 'params.name', // accessor is the "key" in the data
         },
         {
         Header: 'Total Balance',
-        accessor: 'totalBalance',
+        accessor: 'params.total',
+        },
+        {
+        Header: 'Decimals',
+        accessor: 'params.decimals',
         },
         {
         Header: 'Whitelisted addresses',
@@ -66,25 +66,55 @@ const ASAList = (address) => {
     } = useTable({ columns, data })
     
     useEffect(() => {
+        console.log("ASAList address param ", address);
 
-        // axios.get('https://www.reddit.com/r/reactjs.json')
-        // .then(res => {
-        //     const newPosts = res.data.data.children
-        //     .map(obj => obj.data);
-        //     setPosts(newPosts);
+        axios.get('https://api.testnet.algoexplorer.io/idx2/v2/accounts/' + algoSignerContext.currentAddress)
+        .then((res) =>
+        {
+            console.log(res.data.account);
+            setAccountDetails(res.data.account);
+            setCreatedAssets(res.data.account['created-assets']);
+        })
+        .catch((e) => {
+            console.error(e);
+        });
+
+        console.log(accountDetails['created-assets']);
+
+
+        // if (algoSignerContext.currentAddress =! '') {
+        //     AlgoSdk.getAccountDetailsIndexer(algoSignerContext.currentAddress)
+        //     .then((accountDetails) => {
+        //         console.log(accountDetails);
+        //         setAccountDetails(accountDetails);
+        //     })
+        //     .catch((e) => {
+        //         console.error(e);
+        //     });
+
+        // AlgoSdk.getAccountInformation(algoSignerContext.currentAddress).then((accountDetails) => {
+        //     console.log(accountDetails);
+        //     setAccountDetails(accountDetails);
+        //     const columns = Object.keys(accountDetails);
+        //     console.log(columns);
+        //     accountDetails['created-assets'].map(item => {
+        //         console.log(item.index);
+        //         console.log(item.params.name, item.params.total);
+
+        //         axios.get('https://api.testnet.algoexplorer.io/idx2/v2/assets/13672793/balances')
+        //         .then(res => {
+        //             console.log(res);
+        //             const newBalances = res.data.balances.map(obj => obj.address);
+        //             console.log(newBalances);
+        //             setAccountDetails({balances: newBalances});
+        //         });
+
+        //     });
+
         // });
 
-        AlgoSdk.getAccountInformation(algoSignerContext.currentAddress).then((accountDetails) => {
-            console.log(accountDetails);
-            setAccountDetails(accountDetails);
-            const columns = Object.keys(accountDetails);
-            console.log(columns);
-            accountDetails['created-assets'].map(item => {
-                console.log(item.index);
-                console.log(item.params.name, item.params.total)
-            });
+        
 
-        });
 
         
         // async function fetchASACreated (){
@@ -107,6 +137,8 @@ const ASAList = (address) => {
         // fetchWallet();
 
     }, [algoSignerContext.currentAddress]);
+
+    console.log(accountDetails['created-assets']);
 
     return (
     <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
@@ -145,7 +177,6 @@ const ASAList = (address) => {
                     }}
                     >
                     {cell.render('Cell')}
-                    {accountDetails.amount}
                     
                     </td>
                 )
