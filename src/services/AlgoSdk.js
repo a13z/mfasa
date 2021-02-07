b5ba6245ab7d3d92f23e18811fd689c214db5f52
Alfonso Gonzalez <alfonsog@gmail.com>
2021-02-07 20:30:48 +0100

    Remove unused function

--- src/services/AlgoSdk.js
+++ src/services/AlgoSdk.js
@@ -8,16 +8,6 @@ import sdk from 'algosdk';
 const token = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 const sandboxServer = 'http://localhost';
 const port = 4001;
-
-function base64ToArrayBuffer(base64) {
-  const binary_string = window.atob(base64);
-  const len = binary_string.length;
-  const bytes = new Uint8Array(len);
-  for (let i = 0; i < len; i++) {
-    bytes[i] = binary_string.charCodeAt(i);
-  }
-  return bytes.buffer;
-}
 class AlgoSdk {
   client;
 
