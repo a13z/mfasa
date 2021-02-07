7f3dc7d2838d8f85c5623161ea34be7528f91a45
Alfonso Gonzalez <alfonsog@gmail.com>
2021-02-07 20:16:01 +0100

    Create ASA transaction function and helpers

--- src/services/AlgoSdk.js
+++ src/services/AlgoSdk.js
@@ -1,36 +1,28 @@
 import sdk from 'algosdk';
-import axios from 'axios';
-
-// const token = "<your-api-token>";
-// const server = "<http://your-sever>";
-// const port = 8080;
 
 // sandbox
-const token =
-  'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+const token = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 const sandboxServer = 'http://localhost';
 const port = 4001;
 
-// const token = {
-//     'X-API-Key': '<PureStake API Key>'
-// };
-
 class AlgoSdk {
   client;
+
   network;
+
   networks = [
     {
       name: 'testnet',
       server: sandboxServer,
       label: 'TESTNET',
-      indexer: 'https://api.testnet.algoexplorer.io/idx2/'
+      indexer: 'http://testnetServer:8980',
     },
     {
       name: 'mainnet',
       server: 'https://mainnet-algorand.api.purestake.io/ps1',
       label: 'MAINNET',
-      indexer: 'https://api.algoexplorer.io/idx2/'
-    }
+      indexer: 'https://api.algoexplorer.io/idx2/',
+    },
   ];
 
   constructor() {
@@ -47,31 +39,13 @@ class AlgoSdk {
     });
   }
 
-  async getAccountDetailsIndexer(address) {
-    console.log('getAccountDetailsIndexer ' + address);
+  getIndexer() {
     const network = this.getCurrentNetwork();
-    let accountDetails;
-
-    axios
-      .get(network + '/v2/accounts/' + address)
-      .then((res) => {
-        accountDetails = res.data.account;
-        console.log(res.data.account);
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-
-    return accountDetails;
-  }
-
-  getExplorer() {
-    const network = this.getCurrentNetwork();
-    return this.network.explorer;
+    return this.network.indexer;
   }
 
   getAssetUrl(id) {
-    return this.getExplorer() + '/asset/' + id;
+    return `${this.getExplorer()}/asset/${id}`;
   }
 
   getCurrentNetwork() {
@@ -79,7 +53,6 @@ class AlgoSdk {
   }
 
   setClient(server) {
-    // this.client = new sdk.Algod(token, server, port);
     this.client = new sdk.Algodv2(token, server, port);
   }
 
@@ -105,166 +78,165 @@ class AlgoSdk {
       firstRound: 0,
       lastRound: 0,
       genID: '',
-      genHash: ''
+      genHash: '',
     };
 
-    let params = await this.getClient().getTransactionParams();
+    const params = await this.getClient().getTransactionParams().do();
+    console.log('Algorand suggested parameters: %o', params);
+
     cp.firstRound = params.lastRound;
     cp.lastRound = params.lastRound + parseInt(1000);
-    let sFee = await this.getClient().suggestedFee();
-    cp.fee = sFee.fee;
+    cp.fee = params.fee;
     cp.genID = params.genesisID;
-    cp.genHash = params.genesishashb64;
+    cp.genHash = params.genesisHash;
 
     return cp;
   }
 
-  async waitForConfirmation(txId) {
-    let lastRound = (await this.getClient().status()).lastRound;
-    while (true) {
-      const pendingInfo = await this.getClient().pendingTransactionInformation(
-        txId
-      );
-      if (pendingInfo.round !== null && pendingInfo.round > 0) {
-        //Got the completed Transaction
-        console.log(
-          'Transaction ' +
-            pendingInfo.tx +
-            ' confirmed in round ' +
-            pendingInfo.round
-        );
-        break;
-      }
-      lastRound++;
-      await this.getClient().statusAfterBlock(lastRound);
-    }
+  async getSuggestedTxParams() {
+    const params = {
+      fee: 0,
+      firstRound: 0,
+      lastRound: 0,
+      genesisID: '',
+      genesisHash: '',
+    };
+
+    const txParams = await this.getClient().getTransactionParams().do();
+    console.log('Algorand suggested tx parameters: %o', params);
+
+    params.firstRound = txParams.firstRound;
+    params.lastRound = txParams.lastRound;
+    params.fee = txParams.fee;
+    params.genesisID = txParams.genesisID;
+    params.genesisHash = txParams.genesisHash;
+
+    return params;
   }
 
   // Function used to wait for a tx confirmation
-  async waitForConfirmationV2(txId) {
-    let response = await this.status().do();
+  async waitForConfirmation(txId) {
+    const response = await this.getClient().status().do();
     let lastround = response['last-round'];
+    let confirmedRound;
     while (true) {
-      const pendingInfo = await this.pendingTransactionInformation(txId).do();
-      if (
-        pendingInfo['confirmed-round'] !== null &&
-        pendingInfo['confirmed-round'] > 0
-      ) {
-        //Got the completed Transaction
-        console.log(
-          'Transaction ' +
-            txId +
-            ' confirmed in round ' +
-            pendingInfo['confirmed-round']
-        );
+      const pendingInfo = await this.getClient().pendingTransactionInformation(txId).do();
+      if (pendingInfo['confirmed-round'] !== null && pendingInfo['confirmed-round'] > 0) {
+        // Got the completed Transaction
+        confirmedRound = pendingInfo['confirmed-round'];
+        console.log(`Transaction ${txId} confirmed in round ${pendingInfo['confirmed-round']}`);
         break;
       }
       lastround++;
-      await sdk.statusAfterBlock(lastround).do();
+      await this.getClient().statusAfterBlock(lastround).do();
+    }
+    return confirmedRound;
+  }
+
+  // Function used to print created asset for account and assetid
+  async printCreatedAsset(account, assetid) {
+    // note: if you have an indexer instance available it is easier to just use this
+    //     let accountInfo = await indexerClient.searchAccounts()
+    //    .assetID(assetIndex).do();
+    // and in the loop below use this to extract the asset for a particular account
+    // accountInfo['accounts'][idx][account]);
+    const accountInfo = await this.getClient().accountInformation(account).do();
+    for (let idx = 0; idx < accountInfo['created-assets'].length; idx++) {
+      const scrutinizedAsset = accountInfo['created-assets'][idx];
+      if (scrutinizedAsset.index == assetid) {
+        console.log(`AssetID = ${scrutinizedAsset.index}`);
+        const myparms = JSON.stringify(scrutinizedAsset.params, undefined, 2);
+        console.log(`parms = ${myparms}`);
+        break;
+      }
+    }
+  }
+
+  // Function used to print asset holding for account and assetid
+  async printAssetHolding(account, assetid) {
+    // note: if you have an indexer instance available it is easier to just use this
+    //     let accountInfo = await indexerClient.searchAccounts()
+    //    .assetID(assetIndex).do();
+    // and in the loop below use this to extract the asset for a particular account
+    // accountInfo['accounts'][idx][account]);
+    const accountInfo = await this.getClient().accountInformation(account).do();
+    for (let idx = 0; idx < accountInfo.assets.length; idx++) {
+      const scrutinizedAsset = accountInfo.assets[idx];
+      if (scrutinizedAsset['asset-id'] == assetid) {
+        const myassetholding = JSON.stringify(scrutinizedAsset, undefined, 2);
+        console.log(`assetholdinginfo = ${myassetholding}`);
+        break;
+      }
     }
   }
 
   async sendTransaction(rawSignedTxn) {
-    let rawSignedTxnD = sdk.decodeObj(
-      new Uint8Array(Buffer.from(rawSignedTxn, 'base64'))
-    );
-    let transaction = await this.getClient().sendRawTransaction(rawSignedTxnD);
+    // Algosigner NOTE: Due to limitations in Chrome internal messaging, AlgoSigner
+    // encodes the transaction blob in base64 strings.
+    // This is why we have to decode and encode before sendRawTransaction
+    const rawSignedTxnD = sdk.decodeObj(new Uint8Array(Buffer.from(rawSignedTxn, 'base64')));
+
+    console.log('Algosigner Txn decoded');
+    console.log(rawSignedTxnD);
+
+    const rawSignedTxnE = sdk.encodeObj(rawSignedTxnD);
+
+    console.log('Txn encoded with Algosdk');
+    console.log(rawSignedTxnE);
+
+    const transaction = await this.getClient().sendRawTransaction(rawSignedTxnE).do();
+    console.log(transaction);
+
     return transaction;
   }
 
-  async createAssetOld(
-    wallet,
+  async createAssetTxn(
+    sender,
     assetName,
-    unitName,
-    supply,
+    assetUnitName,
+    assetTotal,
+    assetDecimals,
     assetURL,
-    managerAddress,
-    reserveAddress,
-    freezeAddress,
-    clawbackAddress,
-    decimals
+    assetMetadataHash,
+    assetDefaultFrozen,
+    assetManager,
+    assetReserve,
+    assetFreeze,
+    assetClawback,
   ) {
-    let cp = await this.getChangingParams();
+    const params = await this.getSuggestedTxParams();
 
-    let note = new Uint8Array(Buffer.from('MFASA', 'base64'));
-    let addr = wallet;
-    let defaultFrozen = false;
+    // const note = new Uint8Array(Buffer.from('MFASA', 'base64'));
+    const note = sdk.encodeObj('MFASA');
 
-    if (decimals === undefined || decimals === null || decimals === '') {
-      decimals = 0;
+    if (assetDecimals === undefined || assetDecimals === null || assetDecimals === '') {
+      assetDecimals = 0;
     }
-    const txn = sdk.makeAssetCreateTxn(
-      addr,
-      cp.fee,
-      cp.firstRound,
-      cp.lastRound,
-      note,
-      cp.genHash,
-      cp.genID,
-      supply,
-      decimals,
-      defaultFrozen,
-      managerAddress,
-      reserveAddress,
-      freezeAddress,
-      clawbackAddress,
-      unitName,
+
+    const txn = {
+      from: sender,
       assetName,
-      assetURL
-    );
+      assetUnitName,
+      assetTotal,
+      assetDecimals,
+      // note,
+      type: 'acfg',
+      fee: params.fee,
+      // fee: 1000,
+      // flatFee: true,
+      firstRound: params.firstRound,
+      lastRound: params.lastRound,
+      genesisID: params.genesisID,
+      genesisHash: params.genesisHash,
+      assetURL,
+      assetMetadataHash,
+      assetDefaultFrozen,
+      assetManager,
+      assetReserve,
+      assetFreeze,
+      assetClawback,
+    };
 
-    let rawSignedTxn = txn.signTxn(wallet.secretKey);
-
-    let transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
-      'Content-Type': 'application/x-binary'
-    });
-    return transaction;
-  }
-
-  async createAsset(
-    wallet,
-    assetName,
-    unitName,
-    supply,
-    assetURL,
-    managerAddress,
-    reserveAddress,
-    freezeAddress,
-    clawbackAddress,
-    decimals
-  ) {
-    let cp = await this.getChangingParams();
-
-    let note = new Uint8Array(Buffer.from('MFASA', 'base64'));
-    let addr = wallet;
-    let defaultFrozen = false;
-
-    if (decimals === undefined || decimals === null || decimals === '') {
-      decimals = 0;
-    }
-    const txn = sdk.makeAssetCreateTxn(
-      addr,
-      cp.fee,
-      cp.firstRound,
-      cp.lastRound,
-      note,
-      cp.genHash,
-      cp.genID,
-      supply,
-      decimals,
-      defaultFrozen,
-      managerAddress,
-      reserveAddress,
-      freezeAddress,
-      clawbackAddress,
-      unitName,
-      assetName,
-      assetURL
-    );
-
-    // let rawSignedTxn = txn.signTxn(wallet.secretKey);
-
-    // let transaction = (await this.getClient().sendRawTransaction(rawSignedTxn, {'Content-Type': 'application/x-binary'}));
     return txn;
   }
 
@@ -274,12 +246,12 @@ class AlgoSdk {
     managerAddress,
     reserveAddress,
     freezeAddress,
-    clawbackAddress
+    clawbackAddress,
   ) {
-    let cp = await this.getChangingParams();
+    const cp = await this.getChangingParams();
 
-    let note = new Uint8Array(Buffer.from('MFASA', 'base64'));
-    let addr = wallet.address;
+    const note = new Uint8Array(Buffer.from('MFASA', 'base64'));
+    const addr = wallet.address;
     let txn;
     try {
       txn = sdk.makeAssetConfigTxn(
@@ -294,16 +266,16 @@ class AlgoSdk {
         managerAddress,
         reserveAddress,
         freezeAddress,
-        clawbackAddress
+        clawbackAddress,
       );
     } catch (e) {
       console.log(e);
     }
 
-    let rawSignedTxn = txn.signTxn(wallet.secretKey);
+    const rawSignedTxn = txn.signTxn(wallet.secretKey);
 
-    let transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
-      'Content-Type': 'application/x-binary'
+    const transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
+      'Content-Type': 'application/x-binary',
     });
     return transaction;
   }
@@ -313,11 +285,11 @@ class AlgoSdk {
   }
 
   async destroyAsset(wallet, assetId) {
-    let cp = await this.getChangingParams();
+    const cp = await this.getChangingParams();
     const addr = wallet.address;
-    let note = new Uint8Array(Buffer.from('MFASA', 'base64'));
+    const note = new Uint8Array(Buffer.from('MFASA', 'base64'));
 
-    let txn = sdk.makeAssetDestroyTxn(
+    const txn = sdk.makeAssetDestroyTxn(
       addr,
       cp.fee,
       cp.firstRound,
@@ -325,25 +297,25 @@ class AlgoSdk {
       note,
       cp.genHash,
       cp.genID,
-      assetId
+      assetId,
     );
-    let rawSignedTxn = txn.signTxn(wallet.secretKey);
+    const rawSignedTxn = txn.signTxn(wallet.secretKey);
 
-    let transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
-      'Content-Type': 'application/x-binary'
+    const transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
+      'Content-Type': 'application/x-binary',
     });
     return transaction;
   }
 
   async freezeAsset(wallet, assetId, freezeAddress, freezeState) {
-    let cp = await this.getChangingParams();
+    const cp = await this.getChangingParams();
     const addr = wallet.address;
-    let note = new Uint8Array(Buffer.from('MFASA', 'base64'));
+    const note = new Uint8Array(Buffer.from('MFASA', 'base64'));
     if (!freezeState) {
       freezeState = false;
     }
 
-    let txn = sdk.makeAssetFreezeTxn(
+    const txn = sdk.makeAssetFreezeTxn(
       addr,
       cp.fee,
       cp.firstRound,
@@ -353,22 +325,22 @@ class AlgoSdk {
       cp.genID,
       assetId,
       freezeAddress,
-      freezeState
+      freezeState,
     );
 
-    let rawSignedTxn = txn.signTxn(wallet.secretKey);
+    const rawSignedTxn = txn.signTxn(wallet.secretKey);
 
-    let transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
-      'Content-Type': 'application/x-binary'
+    const transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
+      'Content-Type': 'application/x-binary',
     });
     return transaction;
   }
 
   async sendAssets(wallet, assetId, recipient, amount) {
-    let cp = await this.getChangingParams();
+    const cp = await this.getChangingParams();
 
-    let note = new Uint8Array(Buffer.from('MFASA', 'base64'));
-    let sender = wallet.address;
+    const note = new Uint8Array(Buffer.from('MFASA', 'base64'));
+    const sender = wallet.address;
     const revocationTarget = undefined;
     const closeRemainderTo = undefined;
 
@@ -384,13 +356,13 @@ class AlgoSdk {
       note,
       cp.genHash,
       cp.genID,
-      assetId
+      assetId,
     );
 
-    let rawSignedTxn = txn.signTxn(wallet.secretKey);
+    const rawSignedTxn = txn.signTxn(wallet.secretKey);
 
-    let transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
-      'Content-Type': 'application/x-binary'
+    const transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
+      'Content-Type': 'application/x-binary',
     });
     return transaction;
   }
@@ -400,12 +372,12 @@ class AlgoSdk {
     assetId,
     revokeAddress,
     revokeReceiverAddress,
-    revokeAmount
+    revokeAmount,
   ) {
-    let cp = await this.getChangingParams();
+    const cp = await this.getChangingParams();
 
-    let note = new Uint8Array(Buffer.from('MFASA', 'base64'));
-    let sender = wallet.address;
+    const note = new Uint8Array(Buffer.from('MFASA', 'base64'));
+    const sender = wallet.address;
     const revocationTarget = revokeAddress;
     const closeRemainderTo = undefined;
 
@@ -421,13 +393,13 @@ class AlgoSdk {
       note,
       cp.genHash,
       cp.genID,
-      assetId
+      assetId,
     );
 
-    let rawSignedTxn = txn.signTxn(wallet.secretKey);
+    const rawSignedTxn = txn.signTxn(wallet.secretKey);
 
-    let transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
-      'Content-Type': 'application/x-binary'
+    const transaction = await this.getClient().sendRawTransaction(rawSignedTxn, {
+      'Content-Type': 'application/x-binary',
     });
     return transaction;
   }
