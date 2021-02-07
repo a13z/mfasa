b94ac1b463b591181c327e08c54704b4cd10ead5
Alfonso Gonzalez <alfonsog@gmail.com>
2021-02-07 20:28:33 +0100

    Create ASA form working. TODO: loader component

--- src/components/AsaForm/ASAForm.component.jsx
+++ src/components/AsaForm/ASAForm.component.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import React, { useState, useContext, useEffect } from 'react';
+import { useForm, Controller, FormProvider } from 'react-hook-form';
 // import Header from "./Header";
 // import ReactDatePicker from "react-datepicker";
 // import NumberFormat from "react-number-format";
@@ -7,30 +7,25 @@ import { useForm, Controller } from 'react-hook-form';
 // import options from "./constants/reactSelectOptions";
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-
 import {
   TextField,
   Checkbox,
+  Container,
   Button,
   Select,
   MenuItem,
   Switch,
   RadioGroup,
   FormControlLabel,
+  LinearProgress,
   ThemeProvider,
   Radio,
   createMuiTheme,
   Slider,
 } from '@material-ui/core';
+import AlgoSignerContext from '../../contexts/algosigner.context';
 
-import { createAsset } from '../../services/AlgoSdkSandbox';
-
-// import MuiAutoComplete from "./MuiAutoComplete";
-// import "react-datepicker/dist/react-datepicker.css";
-
-// import ButtonsResult from "./ButtonsResult";
-// import DonwShift from "./DonwShift";
-
+import AlgoSdk from '../../services/AlgoSdk';
 // import "./ASAForm.style.scss";
 
 const useStyles = makeStyles((theme) => ({
@@ -47,163 +42,231 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const createTxASA = (values) => {
-  console.log(values);
-  createAsset('', values.assetName, values.unitName, parseInt(values.totalSupply), values.assetUrl, values.managerAddress, values.reserveAddress, values.freezeAddress, values.clawbackAddress, parseInt(values.decimals));
-};
-
-// AlgoSdk.createAsset("Y7DTZHSVUSPZNZ4N5ODX3NPO3SSGE37WCWIHGA7OFDMUCEU3DIVHYHNF2U",
-//   values.assetName,
-//   values.unitName,
-//   values.totalSupply,
-//   values.assetURL,
-//   values.managerAddress,
-//   values.reserveAddress,
-//   values.freezeAddress,
-//   values.clawbackAddress,
-//   values.decimals).then((txn) => {
-//     txnToSign = txn;
-//     txnToSign.from = "Y7DTZHSVUSPZNZ4N5ODX3NPO3SSGE37WCWIHGA7OFDMUCEU3DIVHYHNF2U";
-//     //
-//     delete txnToSign.name;
-//     delete txnToSign.tag;
-//     delete txnToSign.appArgs;
-
-//     console.log(txnToSign);
-
-//   }).catch(error => {
-//     console.log(error);
-//   })
-
-// AlgoSdk.getChangingParams().then((txParams) => {
-//   asTxn = {
-//     from: values.managerAddress,
-//     assetName: values.assetName,
-//     assetUnitName: values.unitName,
-//     assetTotal: values.totalSupply,
-//     assetDecimals: values.decimals,
-//     type: 'acfg',
-//     fee: txParams.fee,
-//     firstRound: txParams.firstRound,
-//     lastRound: txParams.lastRound,
-//     genesisID: txParams.genID,
-//     genesisHash: txParams.genHash
-//   }
-
-//   console.log('Txn changing params' + JSON.stringify(asTxn, null, 2));
-
-//   AlgoSigner.sign(asTxn)
-//   .then((d) => {
-//     signedTx = d;
-//     console.log(JSON.stringify(d, null, 2));
-//     console.log(signedTx.txID, signedTx.blob);
-
-//     AlgoSdk.sendTransaction(d.blob).then((tx) => {
-//       console.log("Transaction : " + tx.txId);
-//     })
-//   })
-//   .catch((e) => {
-//     console.error(e);
-//   });
-
-// }).catch(error => {
-//   console.log(error);
-// })
-
 const ASAForm = ({ asaId }) => {
+  const ctx = useContext(AlgoSignerContext);
+
   const classes = useStyles();
-  const [state, setState] = useState({
-    defaultFrozen: false,
-    defaultSender: false,
-  });
-  const { register, handleSubmit, errors } = useForm();
+  const [defaultFrozen, setDefaultFrozen] = useState(false);
+  const [defaultSender, setDefaultSender] = useState(false);
+  const [managerAddress, setManagerAddress] = useState();
+  const [freezeAddress, setFreezeAddress] = useState();
+  const [reserveAddress, setReserveAddress] = useState();
+  const [clawbackAddress, setClawbackAddress] = useState();
+
   const [submitted, setSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [asaTxn, setAsaTxn] = useState({
+    txId: null,
+    confirmed: false,
+    round: 0,
+  });
 
-  const handleChange = (event) => {
-    console.log(event);
-    setState({ ...state, [event.target.name]: event.target.checked });
+  const {
+    register,
+    handleSubmit,
+    setError,
+    errors,
+    reset,
+    formState: { isSubmitting },
+  } = useForm();
+
+  const createASATx = (values) => {
+    const { AlgoSigner } = window;
+    console.log(values);
+
+    // Create transaction
+    AlgoSdk.createAssetTxn(
+      ctx.currentAddress,
+      values.assetName,
+      values.unitName,
+      parseInt(values.totalSupply),
+      parseInt(values.decimals),
+      values.assetUrl,
+      values.metadata,
+      values.defaultFrozen,
+      values.managerAddress,
+      values.reserveAddress,
+      values.freezeAddress,
+      values.clawbackAddress,
+    ).then((txnToSign) => {
+      console.log('Transaction createAssetTxn');
+      console.log(txnToSign);
+      AlgoSigner.sign(txnToSign)
+        .then((signedTxn) => {
+          console.log('Signing txn');
+          console.log(JSON.stringify(signedTxn, null, 2));
+          console.log(signedTxn.txID, signedTxn.blob);
+
+          AlgoSdk.sendTransaction(signedTxn.blob).then((txnSent) => {
+            console.log('Txn signed sent');
+            console.log(txnSent);
+            AlgoSdk.waitForConfirmation(txnSent.txId).then((response) => {
+              setAsaTxn(
+                {
+                  txId: txnSent.txId,
+                  confirmed: true,
+                  round: response,
+                },
+              );
+              console.log(response);
+            });
+          });
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    }).catch((error) => {
+      console.log(error);
+    });
   };
 
-  const onSubmit = (data) => createTxASA(data);
-  console.log(errors);
+  const handleDefaultSenderChange = (event) => {
+    setDefaultSender(event.target.checked);
 
-  return (
-    <div className={classes.root}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <TextField fullWidth type="text" placeholder="Asset Name" name="assetName" inputRef={register({ required: true })} />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField fullWidth type="text" placeholder="Asset Unit" name="assetUnit" inputRef={register({ required: true })} />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField fullWidth type="number" placeholder="Total Supply" name="totalSupply" inputRef={register({ required: true })} />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField fullWidth type="number" placeholder="Decimals" name="decimals" inputRef={register({ required: true, min: 0 })} />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField fullWidth type="url" placeholder="Asset Url" name="assetUrl" inputRef={register} />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField fullWidth type="text" placeholder="Metadata" name="metadata" inputRef={register({ maxLength: 32 })} />
-          </Grid>
+    if (event.target.checked) {
+      setManagerAddress(ctx.currentAddress);
+      setFreezeAddress(ctx.currentAddress);
+      setReserveAddress(ctx.currentAddress);
+      setClawbackAddress(ctx.currentAddress);
+    } else {
+      setManagerAddress();
+      setFreezeAddress();
+      setReserveAddress();
+      setClawbackAddress();
+    }
+  };
 
-          <Grid item xs={12}>
-            <FormControlLabel
-              control={(
-                <Switch
-                  checked={state.defaultFrozen}
-                  onChange={handleChange}
-                  name="defaultFrozen"
-                  inputRef={register}
-                  color="primary"
-                />
-              )}
-              label="Frozen by default"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControlLabel
-              control={(
-                <Switch
-                  checked={state.defaultSender}
-                  onChange={handleChange}
-                  name="defaultSender"
-                  inputRef={register}
-                  color="primary"
-                />
-              )}
-              label="Default to Sender"
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField fullWidth type="text" placeholder="Manager Address" name="managerAddress" inputRef={register({ required: true, minLength: 58, maxLength: 58 })} />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField fullWidth type="text" placeholder="Reserve Address" name="reserveAddress" inputRef={register({ minLength: 58, maxLength: 58 })} />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField fullWidth type="text" placeholder="Freeze Address" name="freezeAddress" inputRef={register({ minLength: 58, maxLength: 58 })} />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField fullWidth type="text" placeholder="Clawback Address" name="clawbackAddress" inputRef={register({ minLength: 58, maxLength: 58 })} />
-          </Grid>
-          <Grid item xs={3} spacing={9}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleSubmit(onSubmit)}
-            >
-              SUBMIT
-            </Button>
-          </Grid>
-        </Grid>
+  const handleDefaultFrozenChange = (event) => {
+    setDefaultFrozen(event.target.checked);
+  };
 
-      </form>
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      setSubmitted(false);
+      createASATx(data);
+      setLoading(false);
+      setSubmitted(true);
+      reset();
+    } catch (error) {
+      setError(
+        'submit',
+        'submitError',
+        `Oops! There seems to be an issue! ${error.message}`,
+      );
+    }
+  };
+
+  const showLinearProgress = (
+    <LinearProgress />
+  );
+
+  const showSubmitError = (msg) => <p className="msg-error">{msg}</p>;
+
+  const showAsaCreatedMessage = (
+    <div className="msg-confirm">
+      <p>Awesome! Asset has been created.</p>
+      <p>
+        TxId:
+        {asaTxn.txId}
+      </p>
+      <p>
+        Round:
+        {asaTxn.round}
+      </p>
+      <button type="button" onClick={() => setSubmitted(false)}>
+        Create another asset?
+      </button>
     </div>
+  );
+
+  const showForm = (
+    <Container>
+      <div className={classes.root}>
+        <form onSubmit={onSubmit}>
+          <Grid container spacing={2}>
+            { loading ? <LinearProgress /> : showLinearProgress }
+            <Grid item xs={6}>
+              <TextField disabled={isSubmitting} fullWidth type="text" placeholder="Asset Name" name="assetName" inputRef={register({ required: true })} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField disabled={isSubmitting} required fullWidth type="text" placeholder="Asset Unit" name="unitName" inputRef={register({ required: true })} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField disabled={isSubmitting} fullWidth type="number" placeholder="Total Supply" name="totalSupply" inputRef={register({ required: true })} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField disabled={isSubmitting} fullWidth type="number" placeholder="Decimals" name="decimals" inputRef={register({ required: true, min: 0 })} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField disabled={isSubmitting} fullWidth type="url" placeholder="Asset Url" name="assetUrl" inputRef={register} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField disabled={isSubmitting} fullWidth type="text" placeholder="Metadata" name="metadata" inputRef={register({ maxLength: 32 })} />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={(
+                  <Switch
+                    checked={defaultFrozen}
+                    onChange={handleDefaultFrozenChange}
+                    name="defaultFrozen"
+                    inputRef={register}
+                    color="primary"
+                  />
+              )}
+                label="Frozen by default"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={(
+                  <Switch
+                    checked={defaultSender}
+                    onChange={handleDefaultSenderChange}
+                    name="defaultSender"
+                    inputRef={register}
+                    color="primary"
+                  />
+              )}
+                label="Default to Sender"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField disabled={isSubmitting} value={managerAddress || ''} fullWidth type="text" placeholder="Manager Address" name="managerAddress" inputRef={register({ required: true, minLength: 58, maxLength: 58 })} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField disabled={isSubmitting} fullWidth type="text" value={reserveAddress || ''} placeholder="Reserve Address" name="reserveAddress" inputRef={register({ minLength: 58, maxLength: 58 })} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField disabled={isSubmitting} fullWidth type="text" value={freezeAddress || ''} placeholder="Freeze Address" name="freezeAddress" inputRef={register({ minLength: 58, maxLength: 58 })} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField disabled={isSubmitting} fullWidth type="text" value={clawbackAddress || ''} placeholder="Clawback Address" name="clawbackAddress" inputRef={register({ minLength: 58, maxLength: 58 })} />
+            </Grid>
+            <Grid item xs={3} spacing={9}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit(onSubmit)}
+                disabled={isSubmitting}
+              >
+                SUBMIT
+              </Button>
+            </Grid>
+          </Grid>
+
+        </form>
+      </div>
+    </Container>
+  );
+  return (
+    <div className="asa create-asa-page">
+      <div className="form-side">
+        {submitted ? showAsaCreatedMessage : showForm}
+      </div>
+    </div>
+
   );
 };
 
