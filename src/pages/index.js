671af151304bcd19a9f4c835611cbfdceaebedcd
Alfonso Gonzalez <alfonsog@gmail.com>
2021-02-14 21:07:47 +0100

    Add loader icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 
 import axios from 'axios';
 
+import { Container } from '@material-ui/core';
 import Layout from '../components/layout';
 import ASAList from '../components/ASAList/ASAList.component';
 import ASATransactions from '../components/ASATransactions/ASATransactions.component';
@@ -12,8 +13,10 @@ import AlgoSdk from '../services/AlgoSdk';
 const IndexPage = () => {
   const ctx = useContext(AlgoSignerContext);
   const [accountDetails, setAccountDetails] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     if (ctx.currentAddress) {
       axios
         .get(
@@ -22,6 +25,7 @@ const IndexPage = () => {
         .then((res) => {
           console.log(res.data.account);
           setAccountDetails(res.data.account);
+          setLoading(false);
         // res.data['created-assets'].map((item) => {
         //   console.log(item.index);
         //   console.log(item.params.name, item.params.total);
@@ -44,15 +48,17 @@ const IndexPage = () => {
     <Layout>
       <title>ASA Overview</title>
       <h2>ASA Overview</h2>
-      { accountDetails['created-assets'] ? (
-        <>
-          <ASAList data={accountDetails['created-assets']} />
-          <h2>Transactions</h2>
-          <ASATransactions asaList={accountDetails['created-assets']} />
-        </>
-      ) : (
-        <h1>Select an account or this account has not created any ASA</h1>
-      )}
+      <Container>
+        { accountDetails['created-assets'] ? (
+          <>
+            <ASAList data={accountDetails['created-assets']} loading={loading} />
+            <h2>Transactions</h2>
+            <ASATransactions asaList={accountDetails['created-assets']} loading={loading} />
+          </>
+        ) : (
+          <h1>Select an account or this account has not created any ASA</h1>
+        )}
+      </Container>
     </Layout>
   );
 };
