b1f0f950933aaf1a828592579357f6943ddbced0
Alfonso Gonzalez <alfonsog@gmail.com>
2021-02-24 14:11:11 +0100

    Use indexer to fetch transactions

--- src/components/ASATransactions/ASATransactions.component.jsx
+++ src/components/ASATransactions/ASATransactions.component.jsx
@@ -1,11 +1,18 @@
 import React, { useState, useContext, useEffect } from 'react';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import MaUTable from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 import { CircularProgress } from '@material-ui/core';
 
 import { useTable } from 'react-table';
 import './react-table.css';
 
-import axios from 'axios';
+import AlgoSdk from '../../services/AlgoSdk';
 
 const ASATransactions = ({ asaList }) => {
   console.log('ASATransactions asaList', asaList);
@@ -15,67 +22,67 @@ const ASATransactions = ({ asaList }) => {
 
   useEffect(() => {
     const asaTransactions = [];
-    console.log('asaList useEffect', asaList);
-    asaList.map((asa) => {
-      const asaId = asa.index;
-      console.log(`asaId ${asaId}`);
-      axios
-        .get(
-          `https://api.testnet.algoexplorer.io/idx2/v2/assets/${asaId}/transactions`,
-        )
-        .then((res) => {
-          console.log(res);
-          setLoading(false);
-          asaTransactions.push(...res.data.transactions);
-          setTransactions(asaTransactions);
-        })
-        .catch((e) => {
-          console.error(e);
-        })
-        .finally(() => {
-          setLoading(false);
-          console.log(asaTransactions);
+
+    Promise.all(asaList.map((asa) => AlgoSdk.indexer.lookupAssetTransactions(asa.index).do()))
+      .then((response) => {
+        console.log('Promise all fetching transactions for asaList');
+        console.log(response);
+        response.forEach((item) => {
+          asaTransactions.push(...item.transactions);
         });
-    });
-  }, []);
+        console.log('ASATransactions: useEffect asaTransactions array:');
+        console.log(asaTransactions);
+        setTransactions(asaTransactions);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.error(e);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [asaList]);
 
   const columns = React.useMemo(
     () => [
-      // {
-      //   Header: 'Asset Id',
-      //   accessor: 'asset-transfer-transaction.asset-id', // accessor is the "key" in the data
-      // },
+      {
+        Header: 'Asset Id',
+        accessor: 'asset-transfer-transaction.asset-id', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Tx Type',
+        accessor: 'tx-type', // accessor is the "key" in the data
+      },
       {
         Header: 'TxId',
         accessor: 'id', // accessor is the "key" in the data
+      },
+      {
+        Header: 'From',
+        accessor: 'sender',
         width: 15,
       },
-      // {
-      //   Header: 'Round',
-      //   accessor: 'confirmed-round', // accessor is the "key" in the data
-      // },
-      // {
-      //   Header: 'From',
-      //   accessor: 'sender',
-      //   width: 15,
-      // },
-      // {
-      //   Header: 'To',
-      //   accessor: 'asset-transfer-transaction.receiver',
-      //   width: 15,
-      // },
-      // {
-      //   Header: 'Amount',
-      //   accessor: 'asset-transfer-transaction.amount',
-      // },
-      // {
-      //   Header: 'Fee',
-      //   accessor: 'fee',
-      // },
-      // {
-      //   Header: 'Round time',
-      //   accessor: 'round-time',
-      // },
+      {
+        Header: 'To',
+        accessor: 'asset-transfer-transaction.receiver',
+        width: 15,
+      },
+      {
+        Header: 'Amount',
+        accessor: 'asset-transfer-transaction.amount',
+      },
+      {
+        Header: 'Fee',
+        accessor: 'fee',
+      },
+      {
+        Header: 'Round',
+        accessor: 'confirmed-round', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Round time',
+        accessor: 'round-time',
+      },
     ],
     [],
   );
@@ -104,37 +111,37 @@ const ASATransactions = ({ asaList }) => {
     <>
       {loading ? <CircularProgress />
         : (
-          <table {...getTableProps()}>
-            <thead>
+          <MaUTable {...getTableProps()}>
+            <TableHead>
               {headerGroups.map((headerGroup) => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
+                <TableRow {...headerGroup.getHeaderGroupProps()}>
                   {headerGroup.headers.map((column) => (
-                    <th
+                    <TableCell
                       {...column.getHeaderProps()}
                     >
                       {column.render('Header')}
-                    </th>
+                    </TableCell>
                   ))}
-                </tr>
+                </TableRow>
               ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
+            </TableHead>
+            <TableBody {...getTableBodyProps()}>
               {rows.map((row) => {
                 prepareRow(row);
                 return (
-                  <tr {...row.getRowProps()}>
+                  <TableRow {...row.getRowProps()}>
                     {row.cells.map((cell) => (
-                      <td
+                      <TableCell
                         {...cell.getCellProps()}
                       >
                         {cell.render('Cell')}
-                      </td>
+                      </TableCell>
                     ))}
-                  </tr>
+                  </TableRow>
                 );
               })}
-            </tbody>
-          </table>
+            </TableBody>
+          </MaUTable>
         )}
     </>
   );
