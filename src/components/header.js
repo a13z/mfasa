fc9a75e191423f38c5a8b16a3a79e891e74a60a7
Alfonso Gonzalez <alfonsog@gmail.com>
2021-02-24 14:14:58 +0100

    header improvements

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,62 @@
-import { Link } from 'gatsby';
-import PropTypes from 'prop-types';
 import React from 'react';
 
+import {
+  AppBar, Toolbar, List, ListItem, ListItemText, IconButton, Container,
+  makeStyles,
+} from '@material-ui/core';
+import { Home } from '@material-ui/icons';
+
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+
+const useStyles = makeStyles({
+  navDisplayFlex: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  linkText: {
+    textDecoration: 'none',
+    textTransform: 'uppercase',
+    color: 'white',
+  },
+  navbarDisplayFlex: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
+
 const ListLink = (props) => (
-  <li style={{ display: 'inline-block', marginRight: '1rem' }}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
+  <Link className={props.className} to={props.to}>{props.children}</Link>
 );
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h3>
-        <ul style={{ listStyle: 'none', float: 'left' }}>
-          <ListLink to="/">{siteTitle}</ListLink>
-          <ListLink to="/asamanager/">ASA manager</ListLink>
-          <ListLink to="/asaconfig/">ASA config</ListLink>
-          <ListLink to="/reports/">Reports</ListLink>
-        </ul>
-      </h3>
-    </div>
-  </header>
-);
+const Header = ({ siteTitle }) => {
+  const classes = useStyles();
+
+  return (
+    <header>
+      <AppBar position="static">
+        <Toolbar>
+          <Container className={classes.navbarDisplayFlex}>
+            <IconButton edge="start" color="inherit" aria-label="home">
+              <Home fontSize="large" />
+            </IconButton>
+            <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
+              <ListItem button>
+                <ListLink className={classes.linkText} to="/">{siteTitle}</ListLink>
+              </ListItem>
+              <ListItem button>
+                <ListLink className={classes.linkText} to="/asaconfig/">ASA config</ListLink>
+              </ListItem>
+              <ListItem button>
+                <ListLink className={classes.linkText} to="/reports/">Reports</ListLink>
+              </ListItem>
+            </List>
+          </Container>
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
