b0955513a179ca9ff1840130ad16ff6021cac5e8
Alfonso Gonzalez <alfonsog@gmail.com>
2021-02-24 14:12:04 +0100

    Reports form

--- src/components/ReportsForm/ReportsForm.components.jsx
+++ src/components/ReportsForm/ReportsForm.components.jsx
@@ -1,165 +1,377 @@
-import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useTable } from 'react-table';
+import React, { useEffect, useState, useContext } from 'react';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+  DatePicker,
+} from '@material-ui/pickers';
 
-import axios from 'axios';
+import {
+  TextField,
+  Container,
+  Button,
+  Grid,
+  InputLabel,
+  Select,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  LinearProgress,
+  MenuItem,
+  Switch,
+} from '@material-ui/core';
 
-function Table({ columns, data }) {
-  // Use the useTable Hook to send the columns and data to build the table
-  const {
-    getTableProps, // table props from react-table
-    getTableBodyProps, // table body props from react-table
-    headerGroups, // headerGroups, if your table has groupings
-    rows, // rows for the table based on the data passed
-    prepareRow, // Prepare the row (this function needs to be called for each row before getting the row props)
-  } = useTable({
-    columns,
-    data,
-  });
-  /*
-    Render the UI for your table
-    - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
-  */
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-				  prepareRow(row);
-				  return (
-  <tr {...row.getRowProps()}>
-    {row.cells.map((cell) => (
-      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-    ))}
-  </tr>
-				  );
-        })}
-      </tbody>
-    </table>
-  );
+import { useForm, Controller, FormProvider } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useSnackbar } from 'notistack';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+import ASATransactionsTable from '../ASATransactionsTable/ASATransactionsTable.component';
+import AlgoSignerContext from '../../contexts/algosigner.context';
+
+import AlgoSdk from '../../services/AlgoSdk';
+
+const useStyles = makeStyles((theme) => createStyles({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  dense: {
+    marginTop: theme.spacing(2),
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  menu: {
+    width: 200,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+// helper for yup transform function
+function emptyStringToNull(value, originalValue) {
+  if (typeof originalValue === 'string' && originalValue === '') {
+    return null;
+  }
+  return value;
 }
 
-export default function ReportsForm() {
-  const { register, handleSubmit, errors } = useForm();
+const validationSchema = yup.object().shape({
+  minBalance: yup
+    .number()
+    .transform(emptyStringToNull)
+    .nullable()
+    .typeError('Amount must be a number'),
+  maxBalance: yup
+    .number()
+    .transform(emptyStringToNull)
+    .nullable()
+    .typeError('Amount must be a number'),
+  asset: yup
+    .number()
+    .typeError('Asset is required'),
+});
+
+const defaultValues = {
+  address: '',
+  asset: 14038595,
+  fromDate: 'new Date().toISOString().substr(0, 10))',
+  maxBalance: null,
+  minBalance: null,
+  toDate: 'new Date().toISOString().substr(0, 10))',
+  transactionType: 'all',
+};
+
+const transactionTypes = [
+  { value: 'all', text: 'All' },
+  { value: 'pay', text: 'Payment' },
+  { value: 'keyreg', text: 'Key Registration' },
+  { value: 'acfg', text: 'Asset Configuration' },
+  { value: 'afrz', text: 'Asset Freeze' },
+  { value: 'axfer', text: 'Asset Transfer' },
+];
+
+const getAccountDetails = async (address) => {
+  const accountDetails = await AlgoSdk.getIndexer().lookupAccountByID(address).do();
+  console.log('Reports: getAccountDetails ');
+  console.log(accountDetails);
+  return accountDetails.account;
+};
+
+export default function ReportsForm(props) {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  const classes = useStyles();
+
+  const ctx = useContext(AlgoSignerContext);
+
+  const [accountDetails, setAccountDetails] = useState({});
+  const [assetList, setAssetList] = useState([]);
   const [transactions, setTransactions] = useState([]);
+  const [response, setResponse] = useState({});
+  const [submitted, setSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [selectedFromDate, setSelectedFromDate] = React.useState();
+  const [selectedToDate, setSelectedToDate] = React.useState();
+
+  const handleFromDateChange = (date) => {
+    setSelectedFromDate(date.toISOString().substr(0, 10));
+  };
+
+  const handleToDateChange = (date) => {
+    setSelectedToDate(date.toISOString().substr(0, 10));
+  };
+
+  const {
+    errors,
+    handleSubmit,
+    register,
+    setError,
+    control,
+    reset,
+    formState: { isSubmitting },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const fetchTransactions = async ({
+    address, asaId = '13672793', fromDate, toDate, minBalance, maxBalance, transactionType,
+  }) => {
+    console.log(asaId);
+    if (transactionType === 'all') {
+      transactionType = '';
+    }
+
+    const transactionsResults = await AlgoSdk.indexer.lookupAssetTransactions(asaId)
+      .address(address)
+      .txType(transactionType)
+      .beforeTime(toDate)
+      .afterTime(fromDate)
+      .currencyGreaterThan(minBalance)
+      .currencyLessThan(maxBalance)
+      .do();
+    console.log(transactionsResults);
+    setResponse(transactionsResults);
+  };
+
   const onSubmit = (data) => {
-    fetchTransactions(data.asaId);
     console.log(data);
+
+    fetchTransactions(data);
   };
 
   console.log(errors);
 
-  const fetchTransactions = (asaId = '13672793') => {
-    console.log(asaId);
-    axios
-      .get(
-        `https://api.testnet.algoexplorer.io/idx2/v2/assets/${asaId}/transactions?limit=10`,
-      )
-      .then((response) => {
-        console.log(response.data.transactions);
-        setTransactions(response.data.transactions);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   useEffect(() => {
-    fetchTransactions();
-  }, []);
-
-  const data = React.useMemo(() => transactions, [transactions]);
-
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Asset Id',
-        accessor: 'asset-transfer-transaction.asset-id', // accessor is the "key" in the data
-        width: 15,
-      },
-      {
-        Header: 'TxId',
-        accessor: 'id', // accessor is the "key" in the data
-        width: 15,
-      },
-      {
-        Header: 'Round',
-        accessor: 'confirmed-round', // accessor is the "key" in the data
-      },
-      {
-        Header: 'From',
-        accessor: 'sender',
-        width: 15,
-      },
-      {
-        Header: 'To',
-        accessor: 'asset-transfer-transaction.receiver',
-        width: 15,
-      },
-      {
-        Header: 'Amount',
-        accessor: 'asset-transfer-transaction.amount',
-      },
-      {
-        Header: 'Fee',
-        accessor: 'fee',
-      },
-      {
-        Header: 'Round time',
-        accessor: 'round-time',
-      },
-    ],
-    [],
-  );
+    setLoading(true);
+    if (ctx.currentAddress) {
+      getAccountDetails(ctx.currentAddress)
+        .then((response) => {
+          setLoading(false);
+          console.log(response);
+          setAccountDetails(response);
+          setAssetList(response['created-assets']);
+        // fetchTransactions();
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    }
+  }, [ctx]);
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="datetime" placeholder="From" name="From" ref={register} />
-        <input type="datetime" placeholder="To" name="To" ref={register} />
-        <input type="text" placeholder="asaId" name="asaId" ref={register} />
-        <select name="ASA" ref={register}>
-          <option value="All">All</option>
-        </select>
-        <select name="Tx Type" ref={register}>
-          <option value="All">All</option>
-          <option value="Payment">Payment</option>
-          <option value="Key Registration">Key Registration</option>
-          <option value="Asset Configuration">Asset Configuration</option>
-          <option value="Asset Freeze">Asset Freeze</option>
-          <option value="Asset Transfer">Asset Transfer</option>
-        </select>
-        <input
-          type="text"
-          placeholder="Account"
-          name="Account"
-          ref={register({ maxLength: 52 })}
-        />
-        <input
-          type="number"
-          placeholder="Balance min"
-          name="Balance min"
-          ref={register}
-        />
-        <input
-          type="number"
-          placeholder="Balance max"
-          name="Balance max"
-          ref={register}
-        />
+    <Container>
+      { loading ? <LinearProgress />
+        : (
+          <div>
+            <form>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
 
-        <input type="submit" />
-      </form>
+                <Grid container spacing={4}>
+                  <Grid item xs={6}>
+                    <Controller
+                      fullWidth
+                      name="fromDate"
+                      control={control}
+                      valueName="selected" // DateSelect value's name is selected
+                      onChange={([selected]) => selected.toISOString().substr(0, 10)}
+                      as={(
+                        <DatePicker
+                          disableToolbar
+                          disableFuture
+                          variant="inline"
+                          format="dd/MM/yyyy"
+                          margin="normal"
+                          id="from-date-picker-inline"
+                          label="From"
+                          value={selectedFromDate}
+                          onChange={handleFromDateChange}
+                          KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                          }}
+                        />
+                      )}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Controller
+                      fullWidth
+                      name="toDate"
+                      control={control}
+                      valueName="selected" // DateSelect value's name is selected
+                      onChange={([selected]) => selected.toISOString().substr(0, 10)}
+                      as={(
+                        <DatePicker
+                          disableToolbar
+                          disableFuture
+                          variant="inline"
+                          format="dd/MM/yyyy"
+                          margin="normal"
+                          id="to-date-picker-inline"
+                          label="To"
+                          value={selectedToDate}
+                          onChange={handleToDateChange}
+                          KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                          }}
+                        />
+                    )}
+                    />
+                  </Grid>
 
-      <Table columns={columns} data={data} />
-    </>
+                  <Grid container spacing={4}>
+                    <Grid item xs={6}>
+                      <FormControl fullWidth>
+                        <InputLabel
+                          htmlFor="aset-select"
+                          id="asset-input-label"
+                        >
+                          Select Asset
+                        </InputLabel>
+                        <Controller
+                          control={control}
+                          name="asset"
+                          defaultValue=""
+                          as={(
+                            <Select
+                              id="asset-select"
+                            >
+                              {assetList.map((asset) => (
+                                <MenuItem key={asset.index} value={asset.index}>
+                                  {asset.params.name}
+                                </MenuItem>
+                              ))}
+                            </Select>
+                      )}
+                        />
+                        {!!errors.asset && <FormHelperText>Select an Asset</FormHelperText>}
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <FormControl fullWidth>
+                        <InputLabel htmlFor="transaction-type-select" id="transaction-type-select">
+                          Select Transaction Type
+                        </InputLabel>
+                        <Controller
+                          control={control}
+                          name="transactionType"
+                          defaultValue="all"
+                          as={(
+                            <Select id="transaction-type-select">
+                              {transactionTypes.map((type) => (
+                                <MenuItem key={type.value} value={type.value}>
+                                  {type.text}
+                                </MenuItem>
+                              ))}
+                            </Select>
+                      )}
+                        />
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={4}>
+                    <Grid item xs={12}>
+                      <TextField
+                        label="Address"
+                        type="text"
+                        placeholder="Address"
+                        name="address"
+                        fullWidth
+                        disabled={isSubmitting}
+                        inputRef={register}
+                        error={!!errors.address}
+                        helperText={errors.address ? errors.address.message : ''}
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={4}>
+                    <Grid item xs={6}>
+                      <TextField
+                        label="Min Balance"
+                        type="number"
+                        placeholder="Min Balance"
+                        name="minBalance"
+                        fullWidth
+                        disabled={isSubmitting}
+                        inputRef={register}
+                        error={!!errors.minBalance}
+                        helperText={errors.minBalance ? errors.minBalance.message : ''}
+                      />
+                    </Grid>
+                    <Grid item xs={6}>
+                      <TextField
+                        label="Max Balance"
+                        type="number"
+                        placeholder="Max Balance"
+                        name="maxBalance"
+                        fullWidth
+                        disabled={isSubmitting}
+                        inputRef={register}
+                        error={!!errors.maxBalance}
+                        helperText={errors.maxBalance ? errors.maxBalance.message : ''}
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid container><Grid item /></Grid>
+                  <Grid container>
+                    <Grid item xs={10} />
+                    <Grid item xs={2}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleSubmit(onSubmit)}
+                        fullWidth
+                        disabled={isSubmitting}
+                      >
+                        SUBMIT
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </MuiPickersUtilsProvider>
+            </form>
+          </div>
+        )}
+      {(response.transactions && <ASATransactionsTable transactions={response.transactions} />)}
+    </Container>
   );
 }
