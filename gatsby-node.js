e10f094d589fc541ee110d04e7c3356825bc2f0e
Alfonso Gonzalez <alfonsog@gmail.com>
2021-02-24 14:15:49 +0100

    Add dynamic path for asa manager

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,6 @@
-exports.onCreateWebpackConfig = ({ stage, actions, plugins }) => {
+// const { default: ASAManager } = require('./src/pages/asamanager');
+
+exports.onCreateWebpackConfig = ({ actions, plugins }) => {
   actions.setWebpackConfig({
     plugins: [
       plugins.define({
@@ -6,4 +8,26 @@ exports.onCreateWebpackConfig = ({ stage, actions, plugins }) => {
       }),
     ],
   });
+};
+
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions;
+  const path = require('path');
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/asamanager/)) {
+    page.matchPath = '/asamanager/*';
+
+    // Update the page.
+    createPage(
+      {
+        path: '/asamanager/',
+        matchPath: '/asamanager/:assetId',
+        component: path.resolve('./src/pages/asamanager.js'),
+      },
+    );
+  }
 };
