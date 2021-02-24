8ffdef02070ba9ff7d36b1de37dbcacb814dc543
Alfonso Gonzalez <alfonsog@gmail.com>
2021-02-24 14:14:37 +0100

    Layout improvements

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { CssBaseline } from '@material-ui/core';
 import Header from './header';
 import './layout.css';
 
 const Layout = ({ children }) => (
   <>
+    <CssBaseline />
     <Header siteTitle="MFASA" />
     <main>{children}</main>
     <footer>
