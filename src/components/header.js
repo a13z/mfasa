a4aec6c3fa0d7ffeea8c7b57dafbab5dd2da3764
Alfonso Gonzalez <alfonsog@gmail.com>
2021-03-01 14:27:02 +0000

    Add styles from material

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import {
   AppBar, Toolbar, List, ListItem, ListItemText, IconButton, Container,
-  makeStyles,
 } from '@material-ui/core';
+
+import Typography from '@material-ui/core/Typography';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+
 import { Home } from '@material-ui/icons';
 
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   navDisplayFlex: {
     display: 'flex',
     justifyContent: 'space-between',
@@ -19,11 +23,23 @@ const useStyles = makeStyles({
     textTransform: 'uppercase',
     color: 'white',
   },
+  link: {
+    color: 'white',
+    textDecoration: 'none',
+  },
+  root: {
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
   navbarDisplayFlex: {
     display: 'flex',
     justifyContent: 'space-between',
   },
-});
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const ListLink = (props) => (
   <Link className={props.className} to={props.to}>{props.children}</Link>
@@ -33,22 +49,31 @@ const Header = ({ siteTitle }) => {
   const classes = useStyles();
 
   return (
-    <header>
+    <header className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Container className={classes.navbarDisplayFlex}>
-            <IconButton edge="start" color="inherit" aria-label="home">
-              <Home fontSize="large" />
-            </IconButton>
+          <Container className={classes.root}>
             <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
               <ListItem button>
-                <ListLink className={classes.linkText} to="/">{siteTitle}</ListLink>
+                <ListLink className={classes.linkText} to="/">
+                  <Typography className={classes.title} variant="h6" noWrap>
+                    {siteTitle}
+                  </Typography>
+                </ListLink>
               </ListItem>
               <ListItem button>
-                <ListLink className={classes.linkText} to="/asaconfig/">ASA config</ListLink>
+                <ListLink className={classes.linkText} to="/asaconfig/">
+                  <Typography className={classes.title} variant="h6" noWrap>
+                    ASA config
+                  </Typography>
+                </ListLink>
               </ListItem>
               <ListItem button>
-                <ListLink className={classes.linkText} to="/reports/">Reports</ListLink>
+                <ListLink className={classes.linkText} to="/reports/">
+                  <Typography className={classes.title} variant="h6" noWrap>
+                    Reports
+                  </Typography>
+                </ListLink>
               </ListItem>
             </List>
           </Container>
