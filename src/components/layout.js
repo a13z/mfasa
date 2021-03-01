e2e2dd3bd105219bfdec98205cab9596a3fe34d9
Alfonso Gonzalez <alfonsog@gmail.com>
2021-03-01 14:26:48 +0000

    Remove external styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CssBaseline } from '@material-ui/core';
 import Header from './header';
-import './layout.css';
+// import './layout.css';
 
 const Layout = ({ children }) => (
   <>
