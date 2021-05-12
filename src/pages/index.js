4e3c8ac23ae3051016b5be2af443359b20bf645c
Alfonso Gonzalez <alfonsog@gmail.com>
2021-05-12 12:59:12 +0100

    redirect to login initially. clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,13 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { Router } from '@reach/router';
-import { Link } from 'gatsby';
+import { navigate } from 'gatsby';
 import axios from 'axios';
 
 import { Container } from '@material-ui/core';
-import Layout from '../components/Layout';
+
 import AlgoSignerContext from '../contexts/algosigner.context';
 
-// import ASAOverview from './app/asaoverview';
-// import ASAManager from './asamanager';
-// import ASAConfig from './app/asaconfig';
-// import Reports from './reports';
-// import ASAList from '../components/ASAList/ASAList.component';
-
 import Login from '../components/Login';
-import PrivateRoute from '../components/PrivateRoute';
 
 import AlgoClient from '../services/AlgoClient';
 
@@ -24,30 +17,31 @@ const IndexPage = (props) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
-    if (ctx.currentAddress) {
-      const algoClient = new AlgoClient(ctx.ledger);
-      algoClient.getIndexer().lookupAccountByID(ctx.currentAddress).do()
-        .then((response) => {
-          console.log(response);
-          setAccountDetails(response.account);
-          setLoading(false);
-        })
-        .catch((e) => {
-          console.error(e);
-        });
-    }
-  }, [ctx]);
+    navigate('/app/login');
+    // setLoading(true);
+    // if (ctx.currentAddress) {
+    //   const algoClient = new AlgoClient(ctx.ledger);
+    //   algoClient.getIndexer().lookupAccountByID(ctx.currentAddress).do()
+    //     .then((response) => {
+    //       console.log(response);
+    //       setAccountDetails(response.account);
+    //       setLoading(false);
+    //     })
+    //     .catch((e) => {
+    //       console.error(e);
+    //     });
+    // }
+  }, []);
   return (
-    <Layout>
-      {/* <Router>
+    <Login />
+  /* <Router>
         <ASAOverview path="/" />
         <ASAManager path="/asamanager/:assetId" />
         <ASAConfig path="/asaconfig" />
         <Reports path="/reports" />
-      </Router> */}
-      <Link to="/app/login">Login</Link>
-    </Layout>
+      </Router> */
+  //   <Link to="/app/login">Login</Link>
+  // </Layout>
   );
 };
 
